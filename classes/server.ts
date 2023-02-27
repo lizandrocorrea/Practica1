605acc4bc06f14a13362f777cb0232922cc6bd09
lizandro gomez Correa <lizandro.correa@gmail.com>
2023-02-26 22:25:09 -0300

    solo una prueba

--- classes/server.ts
+++ classes/server.ts
@@ -1,3 +1,18 @@
 import express from 'express'
 
-express default class
+export default class Server{
+    public app:express.Application;
+    public port: number = 3000;
+
+    constructor(){
+        this.app=express();
+    }
+
+    Start(callback:Function){
+        this.app.listen(this.port,callback()) 
+    }
+
+
+}
+
+//Creación  de la carpeta Classes 
